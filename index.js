3f1894641cf701c7f702d9df71a738d099b46463
Rahul Naik <naik2.7rahul@gmail.com>
2026-07-23 23:01:56 +0530

    Day 2: Restructured project, added Home and Blog HTML pages with navbar and footer, served via Express static middleware

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World! Express server is running 🚀");
-});
+app.use(express.static(path.join(__dirname, "public")));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
